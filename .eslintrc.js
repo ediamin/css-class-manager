1afa33f053bca2a647626be1232be05a2bb496c8
Edi Amin <to.ediamin@gmail.com>
2023-10-08 21:33:42 +0600

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+const defaultConfig = require( '@wordpress/scripts/config/.eslintrc' );
+
+module.exports = {
+	...defaultConfig,
+	rules: {
+		...defaultConfig.rules,
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: [ 'additional-css-class-manager' ],
+			},
+		],
+		// Heavily using the `__experimental` APIs.
+		'@wordpress/no-unsafe-wp-apis': 'off',
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'parent',
+					'sibling',
+					'index',
+					'type',
+				],
+				pathGroups: [
+					{
+						group: 'external',
+						pattern: '@wordpress/**',
+					},
+				],
+				alphabetize: {
+					caseInsensitive: true,
+					order: 'asc',
+				},
+				pathGroupsExcludedImportTypes: [ 'type' ],
+				'newlines-between': 'always',
+			},
+		],
+		'sort-imports': [
+			'error',
+			{
+				// Handled by the import/order.
+				ignoreDeclarationSort: true,
+			},
+		],
+	},
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require( 'path' );
+
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 function resolve( ...paths ) {
-    return path.resolve( __dirname, ...paths );
+	return path.resolve( __dirname, ...paths );
 }
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    ...defaultConfig,
+	...defaultConfig,
 
-    entry: {
-        'css-class-manager': resolve( 'assets', 'src', 'css-class-manager', 'index.ts' ),
-    },
+	entry: {
+		'css-class-manager': resolve(
+			'assets',
+			'src',
+			'css-class-manager',
+			'index.ts'
+		),
+	},
 
-    output: {
-        filename: '[name]/index.js',
-        path: resolve( 'assets', 'dist' ),
-    },
+	output: {
+		filename: '[name]/index.js',
+		path: resolve( 'assets', 'dist' ),
+	},
 };
