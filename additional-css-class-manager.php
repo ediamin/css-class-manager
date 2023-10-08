04b40b4c92758817bff402bc6d983ca6ccc3faa9
Edi Amin <to.ediamin@gmail.com>
2023-10-08 06:23:10 +0600

    Setup wp-env

---
+++ additional-css-class-manager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Additional CSS Class Manager
+ * Plugin URI: https://github.com/ediamin/additional-css-class-manager
+ * Description: Additional CSS Class Manager for WordPress Blocks.
+ * Version: 0.0.1
+ * Author: Edi Amin
+ * Author URI: https://github.com/ediamin
+ * Text Domain: additional-css-class-manager
+ * Domain Path: /languages/
+ *
+ * @package ediamin/additional-css-class-manager
+ */
+
+declare(strict_types = 1);
+
+// Do not call the file directly.
+defined( 'ABSPATH' ) || exit;
