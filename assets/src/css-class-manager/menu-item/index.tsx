c50e653e76d6beb5f2a70dab96e5db33e36892a2
Edi Amin <to.ediamin@gmail.com>
2024-02-28 12:54:57 +0600

    Add plugin icon

--- assets/src/css-class-manager/menu-item/index.tsx
+++ assets/src/css-class-manager/menu-item/index.tsx
@@ -1,16 +1,16 @@
 import { useDispatch } from '@wordpress/data';
 import { PluginMoreMenuItem } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
-import { settings } from '@wordpress/icons';
 
 import { INTERFACE_STORE, MANAGER_MODAL_NAME } from '../constants';
+import Icon from '../icon';
 
 const MenuItem = () => {
 	const { openModal } = useDispatch( INTERFACE_STORE );
 	const handleOnClick = () => openModal( MANAGER_MODAL_NAME );
 
 	return (
-		<PluginMoreMenuItem icon={ settings } onClick={ handleOnClick }>
+		<PluginMoreMenuItem icon={ Icon } onClick={ handleOnClick }>
 			{ __( 'CSS Class Manager', 'css-class-manager' ) }
 		</PluginMoreMenuItem>
 	);
