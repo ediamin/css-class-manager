7958bf13c476c4ee883fb9a72dca676be3350b15
Edi Amin <to.ediamin@gmail.com>
2025-10-04 13:30:07 +0600

    Update version number to 1.5.0

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -27,7 +27,7 @@ class Plugin
 	/**
 	 * Plugin version number.
 	 */
-	public const VERSION = '1.4.2';
+	public const VERSION = '1.5.0';
 
 	/**
 	 * Plugin ID to use in script handles, actions, filters etc.
